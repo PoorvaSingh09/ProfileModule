49c165b5dd8c79195bd3e3dd54d4953a53539398
Poorva Singh <poorvasingh@Poorvas-MacBook-Pro.local>
2018-07-24 16:16:39 +0530

    Add Login store

--- Home.js
+++ Home.js
@@ -2,12 +2,12 @@ import React, {Component} from 'react'
 import {Text} from 'react-native'
 import {observer, inject} from 'mobx-react'
 
-@inject("navigationStore")
+@inject("loginStore")
 @observer
 export default class Home extends Component {
     render() {
         return(
-            <Text>Welcome {this.props.navigationStore.username}</Text>
+            <Text>Welcome {this.props.loginStore.username}</Text>
         )
     }
 }
